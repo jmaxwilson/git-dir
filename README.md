921365261669e6d26df4c9eabbf6f2f3b3393cef
J. Max Wilson <jmaxwilson@gmail.com>
2019-03-18 14:49:34 -0600

    List repos found and ask before confirmation before running the git command on all of them

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
 # git-dir
-This is a bash script that you can use to run a git command on all of the git repos in a specified directory path. 
+git-dir is a bash script that you can use to run a git command on all of the git repos in a specified directory path.
 
-Copy the script into a directory in your execution PATH and then create an alias:
+## 
 
-`sudo cp git-dir.sh /usr/local/bin`
+`git-dir [git command and options] [path]`
 
-`echo "alias git-dir=/usr/local/bin/git-dir.sh" >> ~/.bash_aliases`
-
-If the specified path is a repo it will run the git commands on that repo.
+If the specified path is a repo, `git-dir` will run the git command on that repo.
 
 `git-dir pull --verbose /dev/repos/mycode`
 
-If the path is not a repo, it will search all of the sub-directories of the path for git repos and run the git command on all of them.
+If the path is not a repo, `git-dir` will search all of the sub-directories in the path for git repos and give you the option to run the git command on all of them.
 
 `git-dir pull /dev/repos/`
+
+## Installation
+
+1. Copy the `git-dir` script into a directory in your execution path:
+
+    `sudo cp git-dir.sh /usr/local/bin`
+
+2. Make sure that it is executable:
+
+    `chmod 744 /usr/local/bin/git-dir.sh`
--- git-dir.sh
+++ git-dir.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # git-dir
 # A useful bash script for running git commands on directories
 if [[ $# -eq 0 ]] ; then # If no arguments are passed offer some guidance 
@@ -9,8 +10,19 @@ else
         cd "$TARGET_PATH" # change to the target path directory
         if [ -d .git ]; then # If the current directory is a git repo then run the git command here
                 git $GIT_ARGS
-        else # If the current directory is not a repo, search subdirs for repos and run the git command on them
-                ls -R --directory --color=never */.git | sed 's/\/.git//' | xargs -P10 -I{} git -C {} $GIT_ARGS
+        else # If the current directory is not a repo, search for subdirs for repos
+                REPO_COUNT=$(ls -R --directory --color=never */.git 2>/dev/null | wc -l)
+                if [ $REPO_COUNT -gt 0 ]; then # If we found repos
+                        printf "Found $REPO_COUNT repositories:\n\n" # List the repos
+                        ls -R --directory --color=never */.git 2>/dev/null | sed 's/\/.git//' && echo ""
+                        read -r -p "Run git $GIT_ARGS on these repos? [Y/n] " CONFIRM #prompt for confirmation
+                        if [[ $CONFIRM =~ ^[Yy]$ || $CONFIRM == "" ]]; then # if Y, y, or [Enter] run git command on each repo
+                                printf "\n"
+                                ls -R --directory --color=never */.git 2>/dev/null | sed 's/\/.git//' | xargs -P10 -I{} git -C {} $GIT_ARGS 
+                        fi
+                else
+                        echo "No repositories found."
+                fi
         fi        
         cd "$INIT_DIR" # return to the directory that we started in
 fi
